def18dcab763ba2afacd790a1d993d22d9e6aef8
Kr1shnam00rth1 <plkrishnamoorthi2004@gmail.com>
2025-03-01 15:08:30 +0530

    Add accessControlMiddleware.js

---
+++ backEnd/middleware/accessControlMiddleware.js
@@ -0,0 +1,27 @@
+const { verifyToken } = require('./jwtMiddleware')
+
+// Function to check user access
+function checkUserAccess(req, res, next){
+    if (req.user.role != 'user'){
+        return res.status(403).json({message: 'Access Denied'});
+    } 
+    next();
+}
+
+// Function to check manager access
+function checkManagerAccess(req, res, next){
+    if (req.user.role != 'manager'){
+        return res.status(403).json({message: 'Access Denied'});
+    } 
+    next();
+}
+
+// Functions to check cashier acess
+function checkCashierAccess(req, res, next){
+    if (req.user.role != 'cashier'){
+        return res.status(403).json({message: 'Access Denied'});
+    } 
+    next();
+}
+
+module.exports = {checkUserAcess, checkManagerAcess, checkCashierAcess};
